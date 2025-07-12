b9bca9b06346990bd9f453ff11dc1dfb24f8bcc1
Banhcanhcua1107 <haidangnakar11@gmail.com>
2025-07-12 15:28:58 +0700

    new update v1

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '@fontsource/be-vietnam-pro/400.css';
 import '@fontsource/be-vietnam-pro/700.css';
-import { ShopProvider } from '/src/components/context/ShopContext';
-import ScrollToTop from '/src/components/router/ScrollToTop';
+import { ShopProvider } from '@/components/context/ShopContext';
+import ScrollToTop from '@/components/router/ScrollToTop';
 function App() {
   return (
     <div className="font-sans bg-[#fffaf3] text-gray-800">
