d4890e71a2434a6eedf2d9c55499976e1b4d51d3
Banhcanhcua1107 <haidangnakar11@gmail.com>
2025-07-12 16:12:09 +0700

    Update new

--- src/components/page/BlogSidebar.jsx
+++ src/components/page/BlogSidebar.jsx
@@ -1,5 +1,5 @@
 import { FaInstagram, FaTwitter, FaFacebookF, FaTumblr, FaVimeoV, FaLinkedinIn } from 'react-icons/fa';
-import Avatar from '@/components/IMG/LOGOCOFFE/bienlaigmail.png';
+import Avatar from '@/components/img/LOGOCOFFE/bienlaigmail.png';
 
 export default function BlogSidebar() {
   return (
